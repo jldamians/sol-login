68e5d546da2dfb9c2b534416b2c9a14501101a3a
Jose Luis Damian Saavedra <jlds161089@gmail.com>
2020-02-19 12:43:59 -0500

    [add] Agregando ejemplo de inicio de sesión incorrecta

---
+++ examples/LoginError1.js
@@ -0,0 +1,25 @@
+const chromium = require('chrome-aws-lambda');
+
+const Signin = require('../lib');
+
+(async() => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    headless: chromium.headless,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  page.goto('https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm', {
+    timeout: 30 * 1000,
+    waitUntil: 'domcontentloaded',
+  });
+
+  const login = new Signin(page);
+
+  login.setKey('239c066cd54b6d090915b616983c4876', '2captcha');
+
+  await login.signin('10729592673', 'MODDATOS', 'MODDATOS');
+})()
