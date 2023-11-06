35a5e6c507860ce5799d49048123cbaf17e50987
jldamians <jdamian@cinternacional.com.pe>
2023-11-06 16:19:18 -0500

    Mejorar el manejo de las esperas

--- examples/index.js
+++ examples/index.js
@@ -2,27 +2,47 @@
 
 import puppeteer from "puppeteer";
 
-import WebsiteLogin from "../lib/index.js";
+import SunatLoginHandler, {TimeConverterUtil} from "../lib/index.js";
 
 (async() => {
+  console.time('scraper');
   const browser = await puppeteer.launch({
     headless: false,
+    slowMo: 5,
+    defaultViewport: {
+      width: 1280,
+      height: 720,
+		},
+    args: [
+      //'--no-sandbox',
+      //'--disable-setuid-sandbox',
+      //'--disable-dev-shm-usage',
+      //'--disable-gpu',
+			'--start-maximized',
+    ],
+    devtools: true,
   });
 
-  const page = await browser.newPage();
+  const [ page ] = await browser.pages();
 
   try {
-    page.goto('https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm', {
-      timeout: 30 * 1000,
-      waitUntil: 'domcontentloaded',
+    const SUNAT_WEBSITE_URL = 'https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm';
+    const MAXIMUM_WAIT_TIME = TimeConverterUtil(20).secsToMillis();
+    page.goto(SUNAT_WEBSITE_URL, {
+      timeout: MAXIMUM_WAIT_TIME,
+      waitUntil: 'networkidle0',
     });
 
-    const perfil = await WebsiteLogin(page).login('10460033280', 'MODDATOS', 'MODDATOS');
+    const perfil = await SunatLoginHandler(page, {
+      visible: true,
+      click: true
+    }).login('10460033280', 'MODDATOS', 'MODDATOS');
 
     console.log(perfil);
   } catch (error) {
-    await browser.close();
-
     console.log(error);
+  } finally {
+    await browser.close();
+    console.timeEnd('scraper');
   }
 })()
