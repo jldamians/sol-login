6bea3a0d6d71f4d0907155b1e9a162f739befeb4
Jose Damian <jldamians@outlook.com>
2023-12-17 16:34:32 -0500

    Anular los pop-up tras el login

--- examples/index.js
+++ examples/index.js
@@ -8,19 +8,22 @@ import SunatLoginHandler, {TimeConverterUtil} from "../lib/index.js";
   console.time('scraper');
   const browser = await puppeteer.launch({
     headless: false,
+    devtools: true,
     slowMo: 5,
     defaultViewport: {
       width: 1280,
       height: 720,
 		},
     args: [
-      //'--no-sandbox',
-      //'--disable-setuid-sandbox',
-      //'--disable-dev-shm-usage',
-      //'--disable-gpu',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
 			'--start-maximized',
+			// These arguments (args) help me consistently locate iframes
+      '--disable-web-security',
+      '--disable-features=IsolateOrigins,site-per-process',
     ],
-    devtools: true,
   });
 
   const [ page ] = await browser.pages();
@@ -28,15 +31,23 @@ import SunatLoginHandler, {TimeConverterUtil} from "../lib/index.js";
   try {
     const SUNAT_WEBSITE_URL = 'https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm';
     const MAXIMUM_WAIT_TIME = TimeConverterUtil(20).secsToMillis();
-    page.goto(SUNAT_WEBSITE_URL, {
+    const REQUEST_INTERCEPTION = true;
+    const ruc = '10460033281';
+    const user = 'MODDATOS';
+    const pass = 'MODDATOS';
+
+    await page.goto(SUNAT_WEBSITE_URL, {
       timeout: MAXIMUM_WAIT_TIME,
       waitUntil: 'networkidle0',
     });
 
+    await page.setRequestInterception(REQUEST_INTERCEPTION);
+
     const perfil = await SunatLoginHandler(page, {
       visible: true,
-      click: true
-    }).login('10460033280', 'MODDATOS', 'MODDATOS');
+      click: true,
+      interception: REQUEST_INTERCEPTION,
+    }).login(ruc, user, pass);
 
     console.log(perfil);
   } catch (error) {
